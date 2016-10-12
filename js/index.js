98c0b3ddb2a54b53d29963365da8f503fda8250c
Arshad Khan <arshdkhn1@gmail.com>
2016-10-12 06:34:25 +0530

    adding scatteerplot

--- js/index.js
+++ js/index.js
@@ -20,109 +20,112 @@ function plot() {
 	var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
 	var graph = d3.select("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
 	.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	// fetching data
 	$.getJSON(url, function(data, status) {
 	if(status !== "success") {
-	alert("data-fetching failed");
-	return;
+		alert("data-fetching failed");
+		return;
 	}
 	else{
-	// doing useful stuff here.
-	var ranks = data.length + (10 - data.length % 10);
-	var timeDiff = -(data[0]["Seconds"]) + (data[data.length - 1]["Seconds"]) + 5;
-	yscale.domain([ranks, 1]);
-	xscale.domain([timeDiff, 0]);
+		// doing useful stuff here.
+		var ranks = data.length + (10 - data.length % 10);
+		var timeDiff = -(data[0]["Seconds"]) + (data[data.length - 1]["Seconds"]) + 5;
+		yscale.domain([ranks, 1]);
+		xscale.domain([timeDiff, 0]);
 
-	// adding axises
-	graph.append("g")
-		.attr("id", "xaxis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xaxis)
-	.append("text")
-		.attr("transform","translate(" + (width - 135) + ", -10)")
-		.attr("x", "15")
-		.attr("class", "xinfo")
-		.text("SECONDS BEHIND FASTEST TIME");
+		// adding axises
+		graph.append("g")
+			.attr("id", "xaxis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xaxis)
+		.append("text")
+			.attr("transform","translate(" + (width - 135) + ", -10)")
+			.attr("x", "15")
+			.attr("class", "xinfo")
+			.text("SECONDS BEHIND FASTEST TIME");
 
-	graph.append("g")
-		.attr("id", "yaxis")
-		.call(yaxis)
-	.append("text")
-		.attr("transform", "rotate(-90)")
-		.attr("y", "15")
-		.attr("class", "yinfo")
-		.text("RANK");
+		graph.append("g")
+			.attr("id", "yaxis")
+			.call(yaxis)
+		.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", "15")
+			.attr("class", "yinfo")
+			.text("RANK");
 
-	var tooltip = d3.select(".tooltip");
-	var bestTimings = data[0]["Seconds"];
-	data.forEach(function(elem) {
-		var x = xscale(elem["Seconds"] - bestTimings);
-		var y = yscale(elem["Place"]);
-		var circle = graph.append("circle")
-							.on("mouseover", function(d) {
-	      				tooltip
-								.style("z-index", "9999")
-								.transition()
-	        			.duration(100)
-	        			.style("opacity", "1")
-								.transition()
-	        			.duration(200)
-								.style("height","200px");
-								$(".name").html(elem["Name"]);
-								$(".place").html(elem["Place"]);
-								$(".time").html(elem["Time"]+" min");
-								$(".year").html(elem["Year"]);
-								$(".nationality").html(elem["Nationality"]);
-								$(".doping").html(elem["Doping"] ? "Has doped" : "Has not doped");
-								tooltip.style("left", (((- d3.event.pageX + $("body").width()) > $("body").width()/2) ?  d3.event.pageX + 50 : d3.event.pageX - 210 )+ "px")
-	        .style("top", (d3.event.pageY + "px"));
-	    				})
-	    				.on("mouseout", function(d) {
-	      				tooltip.transition()
-	        				.duration(200)
-									.style("height","0px")
+		var tooltip = d3.select(".tooltip");
+		var bestTimings = data[0]["Seconds"];
+		data.forEach(function(elem) {
+			var x = xscale(elem["Seconds"] - bestTimings);
+			var y = yscale(elem["Place"]);
+			var circle = graph.append("circle")
+								.on("mouseover", function(d) {
+	        				tooltip
+									.style("z-index", "9999")
 									.transition()
-									.duration(100)
-	        				.style("opacity", "0")
-									.style("z-index", "-999")
-	    				})
-							.on("focus", function() {
-								tooltip
-								.style("z-index", "9999")
+	          			.duration(100)
+	          			.style("opacity", "1")
+									.transition()
+	          			.duration(200)
+									.style("height","auto");
+									$(".name").html(elem["Name"]);
+									$(".place").html(elem["Place"]);
+									$(".time").html(elem["Time"]+" min");
+									$(".year").html(elem["Year"]);
+									$(".nationality").html(elem["Nationality"]);
+									$(".doping").html(elem["Doping"] ? "Has doped" : "Has not doped");
+									tooltip.style("left", (((- d3.event.pageX + $("body").width()) > $("body").width()/2) ?  d3.event.pageX + 30 : d3.event.pageX - 200)+ "px")
+	          .style("top", (d3.event.pageY + "px"));
+	      				})
+	      				.on("mouseout", function(d) {
+	        				tooltip.transition()
+	          				.duration(200)
+										.style("height","0px")
+										.transition()
+										.duration(100)
+	          				.style("opacity", "0")
+										.style("z-index", "-999")
+	      				})
+								.on("focus", function() {
+									tooltip
+									.style("z-index", "9999")
+									.transition()
+	          			.duration(100)
+	          			.style("opacity", "1")
+									.transition()
+	          			.duration(200)
+									.style("height","auto");
+									$(".name").html(elem["Name"]);
+									$(".place").html(elem["Place"]);
+									$(".time").html(elem["Time"]+" min");
+									$(".year").html(elem["Year"]);
+									$(".nationality").html(elem["Nationality"]);
+									$(".doping").html(elem["Doping"] ? "Has doped" : "Has not doped");
+									if((- d3.event.pageX + $("body").width()) > $("body").width()/2)
+										tooltip.style("left", d3.event.pageX + 10);
+									else
+										tooltip.style("right", d3.event.pageX + 10);
+	         				tooltip.style("top", (d3.event.pageY + "px"));
+								})
+								.attr("r", 4.5)
+								.attr("cx", x)
+								.attr("cy", height)
 								.transition()
-	        			.duration(100)
-	        			.style("opacity", "1")
-								.transition()
-	        			.duration(200)
-								.style("height","200px");
-								$(".name").html(elem["Name"]);
-								$(".place").html(elem["Place"]);
-								$(".time").html(elem["Time"]+" min");
-								$(".year").html(elem["Year"]);
-								$(".nationality").html(elem["Nationality"]);
-								$(".doping").html(elem["Doping"] ? "Has doped" : "Has not doped");
-								tooltip.style("left", (((- d3.event.pageX + $("body").width()) > $("body").width()/2) ?  d3.event.pageX + 50 : d3.event.pageX - 210 )+ "px")
-	        .style("top", (d3.event.pageY + "px"));
-							})
-							.attr("r", 4.5)
-							.attr("cx", x)
-							.attr("cy", height)
-							.transition()
-							.duration(1000)
-							.attr("cy", y);
+								.duration(1000)
+								.attr("cy", y);
 
 
-		if(elem["Doping"])
-			circle.attr("class", "doper")
-	});
+			if(elem["Doping"])
+				circle.attr("class", "doper")
+		});
 
-	d3.select("h4")
-		.text("35 Fastest times up Alpe d'Huez");
+		d3.select("h4")
+			.text("35 Fastest times up Alpe d'Huez");
 	}
 	});
 
